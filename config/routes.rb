ba6791a4bc51370b38e5f6073b464c69ec5e7a22
Kurt Fraser <kurtfra@hotmail.com>
2022-08-23 16:07:59 +0200

    CRUD finished

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,15 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  # below is the verb 'get' which grabs
+  # the url 'tasks' (http://localhost:3000/tasks)
+  # which will direct you to the controller 'tasks'
+  # and performs the action 'index'
+  get 'tasks', to: 'tasks#index', as: :tasks
+  get 'tasks/new', to: 'tasks#new', as: :new_task
+  get 'tasks/:id', to: 'tasks#show', as: :task
+  post 'tasks', to: 'tasks#create'
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch 'tasks/:id', to: 'tasks#update'
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
